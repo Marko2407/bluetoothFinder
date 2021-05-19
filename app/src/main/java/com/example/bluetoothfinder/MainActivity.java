cedd6fd71ef7eb9ffd9604886d184f8b17625771
Marko Vukosav <marko.vukosav85@gmail.com>
2021-05-20 00:31:29 +0200

    adding permissions for Internet and fine Location

--- app/src/main/java/com/example/bluetoothfinder/MainActivity.java
+++ app/src/main/java/com/example/bluetoothfinder/MainActivity.java
@@ -1,9 +1,19 @@
 package com.example.bluetoothfinder;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
@@ -17,10 +27,25 @@ public class MainActivity extends AppCompatActivity {
 
     BluetoothAdapter bluetoothAdapter;
 
+    private final BroadcastReceiver receiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            Log.i("Action", action);
+
+            if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)){
+                textView.setText("Done...");
+                button.setEnabled(true);
+
+            }
+        }
+    };
+
     public void find(View view){
 
         textView.setText("Searching...");
         button.setEnabled(false);
+        bluetoothAdapter.startDiscovery();
 
     }
 
@@ -28,10 +53,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION},0);
+        }
 
         textView = findViewById(R.id.statusTextView);
         button = findViewById(R.id.searchButton);
         listView = findViewById(R.id.listView);
+
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(BluetoothAdapter.ACTION_STATE_CHANGED);
+        intentFilter.addAction(BluetoothDevice.ACTION_FOUND);
+        intentFilter.addAction(BluetoothAdapter.ACTION_DISCOVERY_STARTED);
+        intentFilter.addAction(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
+        registerReceiver( receiver, intentFilter);
+
+
+
 
 
     }
